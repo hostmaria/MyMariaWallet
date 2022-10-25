86e0d6a6aa6eddcfd6c9779f4646276cc351c238
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2022-10-25 11:19:24 -0500

    Change Explorer

--- scripts/settings.js
+++ scripts/settings.js
@@ -7,8 +7,7 @@ var networkEnabled = true;
 // A list of Labs-trusted explorers
 const arrExplorers = [
     // Display name      Blockbook-compatible API base
-    { name: "rockdev",   url: "https://explorer.rockdev.org" },
-    { name: "zkBitcoin", url: "https://zkbitcoin.com" }
+    { name: "maria",   url: "https://explorer.mariacoin.com" }
 ]
 
 var cExplorer = arrExplorers[0];
